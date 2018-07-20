8e8fa6a855b86e94b2d14f1353d4f7798e723044
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-07-20 12:55:55 -0700

    adds heapsort solution

--- data_structures/ex_2/heap.py
+++ data_structures/ex_2/heap.py
@@ -1,5 +1,17 @@
 def heapsort(arr):
-  pass
+  # init heap
+  heap = Heap()
+  sorted = []
+
+  for el in arr:
+    heap.insert(el)
+
+  while heap.get_size() > 0:
+    sorted.append(heap.delete())
+
+  sorted.reverse()
+
+  return sorted
 
 class Heap:
   def __init__(self):
